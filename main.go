611708fc4f9498bf8cf91365b546b104430028f7
nitahoria <nitahteodor@gmail.com>
2021-11-08 18:29:00 +0200

    added home get req

--- main.go
+++ main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"github.com/gin-gonic/gin"
 	"hofill/routes"
+	"net/http"
 )
 
 func main() {
@@ -12,6 +13,9 @@ func main() {
 func startAPI() {
 	router := gin.Default()
 	router.GET("/events", routes.GetEvents)
+	router.GET("/", func(c *gin.Context) {
+		c.String(http.StatusOK, "server running")
+	})
 
 	err := router.Run("localhost:8080")
 	if err != nil {
